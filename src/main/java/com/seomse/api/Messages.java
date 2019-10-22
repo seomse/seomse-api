461f6a2710836c6024272f83024b219413abc9a1
macle86 <ysys86a@gmail.com>
2019-10-23 02:56:59 +0900

    기본 메시지 추가

---
+++ src/main/java/com/seomse/api/Messages.java
@@ -0,0 +1,18 @@
+package com.seomse.api;
+/**
+ * <pre>
+ *  파 일 명 : Messages.java
+ *  설    명 : 메시지 종류 정의
+ *
+ *  작 성 자 : macle
+ *  작 성 일 : 2019.10.23
+ *  버    전 : 1.0
+ *  수정이력 :
+ *  기타사항 :
+ * </pre>
+ * @author Copyrights 2019 by ㈜섬세한사람들. All right reserved.
+ */
+public interface Messages {
+    String SUCCESS = "{SUCCESS}";
+    String FAIL = "{FAIL}";
+}
